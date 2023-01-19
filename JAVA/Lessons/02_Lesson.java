614b3bce2cdc2743a2cd445bd44fec1bd2b172fc
Andrii Golik <golik.andrii@gmail.com>
2023-01-19 17:30:35 +0100

    Create 02_Lesson.java

---
+++ JAVA/Lessons/02_Lesson.java
@@ -0,0 +1,20 @@
+// Программа, которая выводит на экран информацию о вас (или выдуманном персонаже) в три строчки.
+// "Меня зовут ..."
+// "Мне ... лет"
+// "Я из ..."
+// На месте многоточий должны использоваться переменные!
+
+package lesson_02;
+
+public class Variables {
+    public static void main(String[] args) {
+        String firstName = "Andrii";
+        String lastName = "Golik";
+        String country = "Ukraine";
+        int age = 44;
+
+        System.out.println("My name is " + firstName + " " + lastName +".");
+        System.out.println("I'm " + age + " years old.");
+        System.out.println("I'm from " + country + ".");
+    }
+}
