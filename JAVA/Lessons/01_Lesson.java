57f11581ddf8e4970e9c61ec6c33275e0d66933b
Andrii Golik <golik.andrii@gmail.com>
2023-01-19 17:29:14 +0100

    Create 01_Lesson.java

---
+++ JAVA/Lessons/01_Lesson.java
@@ -0,0 +1,12 @@
+// Программа, которая выводит на экран информацию о вас (или выдуманном персонаже) в три строчки.
+// "Меня зовут ..."
+// "Мне ... лет"
+// "Я из ..."
+
+public class Main {
+   public static void main(String[] args) {
+     System.out.println("My name is Andrii.");
+     System.out.println("I'm 44 years old.");
+     System.out.println("I'm from Ukraine.");
+   }
+}
