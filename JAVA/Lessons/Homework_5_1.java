0a1974db3dea18d559cf3c071662f0788500bfb3
Andrii Golik <golik.andrii@gmail.com>
2023-01-23 22:27:10 +0100

    Create Homework_5_1.java

---
+++ JAVA/Lessons/Homework_5_1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class MyClass {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Программа вычисления площади прямоугольника");
+        System.out.print("Введите длину прямоугольника в см.: ");
+        int length  = Integer.parseInt(br.readLine());
+        System.out.print("Введите ширину прямоугольника в см.: ");
+        int width  = Integer.parseInt(br.readLine());
+        int square = length * width;
+        System.out.println("________________________________________");
+        System.out.println("Площадь прямоугольника со сторонами " + length + "см. и " + width + "см. равна: " + square + " см"+'\u00B2' );
+    }
+}
