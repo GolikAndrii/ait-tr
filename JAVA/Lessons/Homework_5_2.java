40128c9b6c7b63f831280ee29673c02d2ddafdb0
Andrii Golik <golik.andrii@gmail.com>
2023-01-23 23:00:16 +0100

    Create Homework_5_2.java

---
+++ JAVA/Lessons/Homework_5_2.java
@@ -0,0 +1,25 @@
+//Написать программу, которая считывает с клавиатуры пять символов и выводит их коды.
+
+import java.io.*;
+public class Speed {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+        System.out.println("Введите 5 любых символов на клавитуре: ");
+      
+        int symbol_1 = (br.read());
+        int symbol_2 = (br.read());
+        int symbol_3 = (br.read());
+        int symbol_4 = (br.read());
+        int symbol_5 = (br.read());
+      
+        System.out.println("");
+        System.out.println("Вот коды данных символов: ");
+        System.out.println("_____________________________");
+        System.out.println("Символу '" +(char)symbol_1+ "' соответствует код: " +symbol_1);
+        System.out.println("Символу '" +(char)symbol_2+ "' соответствует код: " +symbol_2);
+        System.out.println("Символу '" +(char)symbol_3+ "' соответствует код: " +symbol_3);
+        System.out.println("Символу '" +(char)symbol_4+ "' соответствует код: " +symbol_4);
+        System.out.println("Символу '" +(char)symbol_5+ "' соответствует код: " +symbol_5);
+    }
+}
