3d9adb62f9bf8f8b06822562094fd12f992e75d7
Andrii Golik <golik.andrii@gmail.com>
2023-01-25 22:03:52 +0100

    Update Homework_7_2.java

--- JAVA/Lessons/Homework_7_2.java
+++ JAVA/Lessons/Homework_7_2.java
@@ -1,3 +1,4 @@
+//  Напишите программу, которая читает строку, левый индекс и правый индекс и выводит получившуюся подстроку.
 import java.io.*;
 
 public class Homework_7_2{
@@ -7,17 +8,17 @@ public class Homework_7_2{
         String text = br.readLine();
 
         int textLength = text.length();
-        System.out.printf("В данной строке всего символов: %s%n", text.length());
+        System.out.printf("В данной строке всего символов: %s%n", textLength);
+        String newText = text.substring(0, textLength);
 
-        char firstElement = text.charAt(0);
-        char lastElement = text.charAt(textLength-1);
+       System.out.printf("Левый индекс  - '%s'%n", 0);
+       System.out.printf("Правый индекс - '%s'%n", textLength-1);
+       System.out.printf("Новая подстрока  - '%s'%n", newText);
 
-        System.out.printf("Первый симовл - '%s'%n", firstElement);
-        System.out.printf("Последний символ - '%s'%n", lastElement);
     }
 }
-
 //  Введите любой текст: I want to be FullStack Developer!
 //  В данной строке всего символов: 33
-//  Первый симовл - 'I'
-//  Последний символ - '!'
+//  Левый индекс  - '0'
+//  Правый индекс - '32'
+//  Новая подстрока  - 'I want to be FullStack Developer!'
