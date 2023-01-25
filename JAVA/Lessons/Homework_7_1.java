b39f0e4a30d6d4492f579d2623a36b1801a10a1b
Andrii Golik <golik.andrii@gmail.com>
2023-01-25 21:36:02 +0100

    Update Homework_7_1.java

--- JAVA/Lessons/Homework_7_1.java
+++ JAVA/Lessons/Homework_7_1.java
@@ -1,14 +1,14 @@
 //  Напишите программу, которая читает строку, и выводит её длину, первый и последний символы.
 import java.io.*;
 
-public class Homework_7_!{
+public class Homework_7_1{
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Введите любой текст: ");
         String text = br.readLine();
 
         int textLength = text.length();
-        System.out.printf("В данной строке всего символов: %s%n", text.length());
+        System.out.printf("В данной строке всего символов: %d%n", textLength);
 
         char firstElement = text.charAt(0);
         char lastElement = text.charAt(textLength-1);
