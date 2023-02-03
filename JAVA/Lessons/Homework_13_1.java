fedef88d9639f5cd0136616eb808d1f07c84acb8
Andrii Golik <golik.andrii@gmail.com>
2023-02-03 14:58:00 +0100

    Update Homework_13_1.java

--- JAVA/Lessons/Homework_13_1.java
+++ JAVA/Lessons/Homework_13_1.java
@@ -1,1 +1,17 @@
-Test
+import java.io.*;
+
+public class Homework_13_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int firstNumber = Integer.parseInt(br.readLine());
+        int secondNumber = Integer.parseInt(br.readLine());
+        int thirdNumber = Integer.parseInt(br.readLine());
+
+        if((firstNumber < secondNumber+thirdNumber) && (secondNumber < firstNumber+thirdNumber) && (thirdNumber < firstNumber+secondNumber)){
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
