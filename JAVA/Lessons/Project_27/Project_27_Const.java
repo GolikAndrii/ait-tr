5ed7357223f5a2733be19fbe6ec43033fc3907fd
Andrii Golik <golik.andrii@gmail.com>
2023-02-25 13:17:49 +0100

    Create Project_27_Const.java

---
+++ JAVA/Lessons/Project_27/Project_27_Const.java
@@ -0,0 +1,9 @@
+package Project_27;
+
+public class Project_27_Const {
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+}
