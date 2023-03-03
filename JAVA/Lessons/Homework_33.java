087f7057693554e50141266c7c332809b592107b
Golik Andrii <Golik.Andrii@gmail.com>
2023-03-03 14:39:33 +0100

    add new method "buildMonthsMap" and put month from Enum to Map

--- JAVA/Lessons/Homework_33.java
+++ JAVA/Lessons/Homework_33.java
@@ -1,3 +1,5 @@
+import Homework_32.Homework_32_1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,6 +27,25 @@ public class Homework_33 {
         OCTOBER,
         NOVEMBER,
         DECEMBER,
+    }
+
+    public static Map<Month, Integer> buildMonthsMap() {
+        Map<Month, Integer> months = new HashMap<>();
+
+        months.put(Month.JANUARY, 31);
+        months.put(Month.FEBRUARY, 28);
+        months.put(Month.MARCH, 31);
+        months.put(Month.APRIL, 30);
+        months.put(Month.MAY, 31);
+        months.put(Month.JUNE, 30);
+        months.put(Month.JULY, 31);
+        months.put(Month.AUGUST, 31);
+        months.put(Month.SEPTEMBER, 30);
+        months.put(Month.OCTOBER, 31);
+        months.put(Month.NOVEMBER, 30);
+        months.put(Month.DECEMBER, 31);
+
+        return months;
     }
 
     public static void main(String[] args) {
