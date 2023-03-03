50fa7f680d18fa70b3c17c903922a85a8e6e2c07
Golik Andrii <Golik.Andrii@gmail.com>
2023-03-03 14:13:23 +0100

    add new enum Season

--- JAVA/Lessons/Homework_33.java
+++ JAVA/Lessons/Homework_33.java
@@ -1,2 +1,21 @@
-public class Homework_32_1 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+public class Homework_33 {
+
+    enum Season{
+        SPRING,
+        SUMMER,
+        AUTUMN,
+        WINTER,
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
 }
